5b7c80ed8ad27970190fdb5f70482c1304e5f3ba
Benje Dev <Jasminlabidi@gmx.de>
2021-11-16 13:52:40 +0100

    update time CSS

--- src/app.js
+++ src/app.js
@@ -1,35 +1,49 @@
-function showTemperature(response) {   
-document.querySelector("#actual-temp").innerHTML = Math.round(response.data.main.temp);
-document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
-document.querySelector("#wind-speed").innerHTML = Math.round((response.data.wind.speed*3.6));
-document.querySelector("#search-city").innerHTML = response.data.name;
-document.querySelector("#weather-description").innerHTML = response.data.weather[0].description;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = (date.getHours() < 10 ? `0` : ``) + date.getHours();
+  let minutes = (date.getMinutes()<10? `0` : ``) + date.getMinutes();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+  let day = days[date.getDay()];
+
+  let sentence = `${day} ${hours}:${minutes}`;
+  console.log(sentence)
 }
 
 
 
+function showTemperature(response) {
+  document.querySelector("#actual-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed * 3.6
+  );
+  document.querySelector("#search-city").innerHTML = response.data.name;
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
 
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
 
-apiKey = "15b6771cede26fdda2ef2045a9e7c815";
+let apiKey = "15b6771cede26fdda2ef2045a9e7c815";
 // city = "Stuttgart";
 // apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Stuttgart&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Stuttgart&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl)
+console.log(apiUrl);
 
 axios.get(apiUrl).then(showTemperature);
-
-
-
-
-
 
 //////////////////////////////////////////
 
 function getCity() {
-  city = document.querySelector("input.value");
+  let city = document.querySelector("input.value");
   console.log(input);
 }
 
-searchForm = document.querySelector("#search-form");
+let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", getCity);
